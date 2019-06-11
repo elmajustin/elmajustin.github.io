2b936362e6c69e06842b2b554e0700ad1dcdb1ce
elizabeth <elizabeth@qburst.com>
2019-06-11 17:04:33 +0530

    files

--- js/custom.js
+++ js/custom.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
     $('.count').each(function () {
     var $this = $(this);
     jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-      duration: 3000,
+      duration: 2000,
       easing: 'swing',
       step: function () {
         $this.text(Math.ceil(this.Counter));
